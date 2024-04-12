7f1f567125a486ef4f3a1c6b852833bbb210aa86
vimaljethva35 <210303105035@PARULUNIVERSITY.AC.IN>
2024-04-12 08:08:24 +0530

    p-8
    p-8

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
-
-// Please create a free database for yourself.
-// This database will be deleted after tutorial
+'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1'
